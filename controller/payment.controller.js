f0cb8d1ee5c7cfbc81a7b07e9bb1f53d516cd313
mang0g0rilla <skymailsenter@gmail.com>
2020-08-10 23:42:00 -0500

    stripe back

--- controller/payment.controller.js
+++ controller/payment.controller.js
@@ -12,9 +12,9 @@ if(process.env.NODE_ENV === 'development'){
 	PK = process.env.STRIPE_PUBLIC_DEV;
 	WH = process.env.STRIPE_WH_DEV;
 } else {
-	stripe = require('stripe')(process.env.STRIPE_SECRET_DEV);
-	PK = process.env.STRIPE_PUBLIC_DEV;
-	WH = process.env.STRIPE_WH_DEV;
+	stripe = require('stripe')(process.env.STRIPE_SECRET);
+	PK = process.env.STRIPE_PUBLIC;
+	WH = process.env.STRIPE_WH;
 }
 
 module.exports.preparePayment = async (req, res, next) => {
