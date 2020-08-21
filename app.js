bd048397afbc0eab735046287f30657f5b3ce132
mang0g0rilla <skymailsenter@gmail.com>
2020-08-21 14:56:30 -0500

    main page update intermediate

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(cookieParser());
 app.use('*/css',express.static('public/css'));
 app.use('*/js',express.static('public/js'));
 app.use('*/images',express.static('public/images'));
+app.use('*/videos',express.static('public/videos'));
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
